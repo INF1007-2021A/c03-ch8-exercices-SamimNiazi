09a56d5f4e547fbd27b5bd98f65b32df9de14cbe
SamimNiazi <samim.niazi@polymtl.ca>
2021-11-02 21:28:44 -0400

    yea i guess

--- exercice.py
+++ exercice.py
@@ -5,11 +5,18 @@ PERCENTAGE_TO_LETTER = {"A*": [95, 101], "A": [90, 95], "B+": [85, 90], "B": [80
 
 # TODO: Importez vos modules ici
 
-
 # TODO: Définissez vos fonction ici
+def lecture_fichier (fichier1, fichier2):
+    
+    with open ("fichier1", 'r') as f:
+        with open ("fichier2",'r') as g:
+            string_du_premier_fichier = f.readlines()
+            string_du_deuxieme_fichier = g.readlines()
+
+
 
 
 if __name__ == '__main__':
     # TODO: Appelez vos fonctions ici
-
+    print(lecture_fichier ("fichier1","fichier2"))
     pass
