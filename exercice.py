51419fd0bcb736758ae0c1a4100dc164829c35d2
SamimNiazi <samim.niazi@polymtl.ca>
2021-11-16 10:20:43 -0500

    1 et 2 fini

--- exercice.py
+++ exercice.py
@@ -3,20 +3,31 @@
 
 PERCENTAGE_TO_LETTER = {"A*": [95, 101], "A": [90, 95], "B+": [85, 90], "B": [80, 85], "C+": [75, 80], "C": [70, 75], "F": [0, 70]}
 
-# TODO: Importez vos modules ici
 
-# TODO: Définissez vos fonction ici
 def lecture_fichier (fichier1, fichier2):
-    
-    with open ("fichier1", 'r') as f:
-        with open ("fichier2",'r') as g:
-            string_du_premier_fichier = f.readlines()
-            string_du_deuxieme_fichier = g.readlines()
+    with open(fichier1, "r", encoding = "utf-8") as file1, open(fichier2, "r", encoding = "utf-8") as file2:
+        caractere_file1 = file1.read(1)
+        caractere_file2 = file2.read(1)
+        
+        while caractere_file1 == caractere_file2:
+            caractere_file1 = file1.read(1)
+            caractere_file2 = file2.read(1)
+
+    return f'La première diffèrence est {caractere_file1} et {caractere_file2}'    
+
+def triple_espace (fichier1, fichier2):
+    with open(fichier1, "r", encoding = "utf-8") as file1, open(fichier2, "w", encoding = "utf-8") as file2:
+        caractere_file1 = file1.read()
+        for lettre in caractere_file1:
+            if lettre != " ":
+                file2.write(lettre)
+            else:
+                file2.write ("   ")
 
 
 
 
 if __name__ == '__main__':
-    # TODO: Appelez vos fonctions ici
     print(lecture_fichier ("fichier1","fichier2"))
+    print(triple_espace("fichier1", "fichier2"))
     pass
